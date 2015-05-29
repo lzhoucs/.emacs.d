4fdad091d95198193e6000acaacaf89b534e8f35
Liang Zhou <lzhoucs@gmail.com>
2015-05-29 00:20:16 -0500

    Exclude package source in .gitignore. Add a few packages and configurations.
    ZL-G580-Freya|Terminal

--- init.el
+++ init.el
@@ -3,12 +3,25 @@
 			 ("marmalade" . "https://marmalade-repo.org/packages/")
 			 ))
 
+; key binding
+(global-set-key (kbd "C-o") 'er/expand-region)
+(global-set-key (kbd "C-c SPC") 'ace-jump-mode)
+
+; function settings
 (menu-bar-mode -1)
 
 (delete-selection-mode t)
 
 (show-paren-mode t)
 
+(ido-mode 1)
+(ido-everywhere 1)
+
+; auto-complete doesn't work for now
+;(require 'auto-complete-config)
+;(ac-config-default)
+
+; variable settings
 (setq inhibit-startup-screen t)
 
 (setq make-backup-files nil)
@@ -17,3 +30,4 @@
 
 
 (fset 'yes-or-no-p 'y-or-n-p)
+
