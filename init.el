d62c45a3c28c08f10152ef4259deabc123cd79ae
Liang Zhou <lzhoucs@gmail.com>
2015-05-28 21:31:23 -0500

    Add a few useful settings.
    ZL-G580-Freya|Terminal

--- init.el
+++ init.el
@@ -1,8 +1,19 @@
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-			 ("marmalade" . "https://marmalade-repo.org/packages/")
 			 ("melpa" . "http://melpa.org/packages/")
+			 ("marmalade" . "https://marmalade-repo.org/packages/")
 			 ))
 
 (menu-bar-mode -1)
 
+(delete-selection-mode t)
+
+(show-paren-mode t)
+
 (setq inhibit-startup-screen t)
+
+(setq make-backup-files nil)
+
+(setq auto-save-default nil)
+
+
+(fset 'yes-or-no-p 'y-or-n-p)
