28e46b256b7e2cfe210e8cc68047ebf34b382556
Liang Zhou <lzhoucs@gmail.com>
2015-06-05 11:53:00 -0500

    Minor typo fix.
    ZL-E5540-Windows7|emacs

--- init.el
+++ init.el
@@ -10,7 +10,7 @@
 ; function settings
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-(if (fboundp 'scroll-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
 (delete-selection-mode t)
 
