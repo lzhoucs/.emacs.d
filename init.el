dfbc525fb67ca95e03325653e1e1a7e7b8385b83
Liang Zhou <Liang.Zhou@availity.com>
2016-02-12 01:13:27 -0600

    Add key binding for mc/skip-to-next-like-this.
    LZHOU-AVVM-Win7

--- init.el
+++ init.el
@@ -112,6 +112,7 @@
 (global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-?") 'mc/skip-to-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
